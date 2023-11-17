5e29637e74dc8fa691d6671a2b88f0cf8849989f
Sabrinavf <contactsabrinafernandes@gmail.com>
2023-11-17 18:54:01 -0300

    Adicionar arquivos de projeto.

---
+++ valorPrduto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estudo
+{
+    class somaSimples
+    {
+        static void Main(string[] args)
+        {
+            int PROD, A, B;
+
+            A = int.Parse (Console.ReadLine());
+            B= int.Parse (Console.ReadLine());
+
+            PROD = A * B;
+
+            Console.WriteLine("PROD = " + PROD);
+
+        }
+    }
+}
